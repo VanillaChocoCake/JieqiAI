e431f23f5fd1e599c6eb51410466c7367be203bc
sunpeihan <sunpeihan@mail.ustc.edu.cn>
2022-04-20 18:53:30 +0800

    小更新

--- main_code/ai/AI_Display_AI_Input.py
+++ main_code/ai/AI_Display_AI_Input.py
@@ -1,6 +1,7 @@
 # 展示代码，AI输入
 import socket
 from SL import *
+import numpy as np
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((host, port))
@@ -10,7 +11,19 @@ while True:
     side = input("红方1，黑方0：")
     while True:
         board_info = client.recv(2048)
-        message = board_info.decode("utf-8").split(" ")
+        message = board_info.decode("utf-8")
+        if message == "end":
+            break
+        message = message.split(" ")
         board = translate_message(message)
+        st = convert_board_to_array(board)
         if side == camp_red:
+            predict = red_sl_model.predict(st)
+            action = np.argmax(predict)
+        else:
+            predict = black_sl_model.predict(st)
+            action = np.argmin(message)
+        decision = convert_board_to_array(action)
+        client.send(decision.encode("utf-8"))
+
 
